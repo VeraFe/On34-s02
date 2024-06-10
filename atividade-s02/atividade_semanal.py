605085833bf279a118d24fbc08c6b544b21e8636
Vera-Felix <verafelix37@gmail.com>
2024-06-10 12:42:38 -0300

    atividade

---
+++ atividade-s02/atividade_semanal.py
@@ -0,0 +1,39 @@
+# somar(x, y): recebe dois números e retorna a soma.
+x = 5
+y = 5
+soma = (x+y)
+eh_impar = soma%2
+eh_impar = bool (eh_impar)
+print ("O numero", soma,"é impar?", bool (eh_impar))
+
+# subtrair(x, y): recebe dois números e retorna a subtração.
+x = 15
+y = 10
+subtracao = (x-y)
+eh_impar = subtracao%2
+eh_impar = bool (eh_impar)
+print ("O numero", subtracao,"é impar?", bool (eh_impar))
+
+# multiplicar(x, y): recebe dois números e retorna a multiplicação.
+x = 25
+y = 5
+multiplicacao = (x*y)
+eh_impar = multiplicacao%2
+eh_impar = bool (eh_impar)
+print ("O numero", multiplicacao,"é impar?", bool (eh_impar))
+
+# dividir(x, y): recebe dois números e retorna a divisão do primeiro pelo segundo
+x = 10
+y = 2
+divisao = (x/y)
+eh_impar = divisao%2
+eh_impar = bool (eh_impar)
+print ("O numero", divisao,"é impar?", bool (eh_impar))
+
+# potenciação(x, y)
+x = 5
+y = 2
+potenciacao = (x**y)
+eh_impar = potenciacao%2
+eh_impar = bool (eh_impar)
+print ("O numero", potenciacao,"é impar?", bool (eh_impar))
